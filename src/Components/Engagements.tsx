ec59d3c59dcdaba04f7f98a4f1170e163016ad1f
ankushnalamwarNV <ankush.nalamwar@newvision-software.com>
2023-11-23 13:04:05 +0530

    Added Download pdf functionality on generate audit report method and Integrated API for Email notification

--- src/Components/Engagements.tsx
+++ src/Components/Engagements.tsx
@@ -27,17 +27,24 @@ export const Engagements = (props: IEngagement) => {
                         <th className="border-2">Status</th>
                         <th className="border-2">Start Date</th>
                         <th className="border-2">End Date</th>
+                        <th className="border-2">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     {props?.tableArray?.map((item: any, index: any) => (
                         <tr key={index}>
-                            <td className="border-2" onClick={() => navigate(`/viewEngagement/${item.clientId}`)}>{item.clientId}</td>
-                            <td className="border-2">{item.clientName}</td>
-                            <td className="border-2">Loading..</td>
-                            <td className="border-2">Loading..</td>
-                            <td className="border-2">{item.engagementStartDate}</td>
-                            <td className="border-2">{item.engagementEndDate}</td>
+                            <td className="border-2 cursor-pointer underline decoration-sky-500 text-center" onClick={() => navigate(`/viewEngagement/${item.clientId}`)}>{item.clientId}</td>
+                            <td className="border-2 text-center">{item.clientName}</td>
+                            <td className="border-2 text-center">Loading..</td>
+                            <td className="border-2 text-center">Loading..</td>
+                            <td className="border-2 text-center">{item.engagementStartDate}</td>
+                            <td className="border-2 text-center">{item.engagementEndDate}</td>
+                            <td className="border-2 text-center">
+                                <svg onClick={() => navigate(`/viewEngagement/${item.clientId}`)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 cursor-pointer mx-auto">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                </svg>
+                            </td>
                             {/* Add more cells based on your data structure */}
                         </tr>
                     ))}
